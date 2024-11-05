764d974c84db9e087854b57acb6734927c33cf02
Anabia <ana.00000850038@unicap.br>
2024-11-05 10:30:11 -0300

    adicao de novas tabelas

--- ProjetoBD/src/DatabaseCreator.java
+++ ProjetoBD/src/DatabaseCreator.java
@@ -6,28 +6,34 @@ import java.sql.Statement;
 public class DatabaseCreator {
 
     // Configurações do MySQL
-    private static final String URL = "jdbc:mysql://localhost:3306/";
+    private static final String URL = "jdbc:mysql://localhost:3306/?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "sua_senha";
+    private static final String PASSWORD = "root";
     
     private Connection connection;
 
     public DatabaseCreator() {
         try {
+            // Carregar o driver MySQL
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
             // Conectar ao MySQL
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
             Statement statement = connection.createStatement();
             
             // Criação do banco de dados
-            String sql = "CREATE DATABASE IF NOT EXISTS minha_base_de_dados";
+            String sql = "CREATE DATABASE IF NOT EXISTS sistema";
             statement.executeUpdate(sql);
             System.out.println("Banco de dados criado com sucesso!");
             
             // Seleciona o banco de dados para uso
-            connection.setCatalog("minha_base_de_dados");
+            connection.setCatalog("sistema");
             
             statement.close();
         } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.err.println("Driver MySQL não encontrado!");
             e.printStackTrace();
         }
     }
