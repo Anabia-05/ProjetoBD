b5a9cf9717e0ef04266b59f2031e45c41e68d216
Anabia-05 <anabia.romero@gmail.com>
2024-11-06 11:55:38 -0300

    adicao de algumas insercoes

--- ProjetoBD/src/util/DataBaseInserter.java
+++ ProjetoBD/src/util/DataBaseInserter.java
@@ -25,4 +25,36 @@ public class DataBaseInserter {
             e.printStackTrace();
         }
     }
+
+    public void insertInstituicao(String nome, String causa_social,  String endereco, int id_plano){
+        String sql = "INSERT INTO instituicao(nome,causa_social,endereco,id_plano) VALUES (?,?,?,?)";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, nome);
+            stmt.setString(2, causa_social);
+            stmt.setString(3, endereco);
+            stmt.setInt(4, id_plano);
+            stmt.executeUpdate();
+            System.out.println("Dados inseridos na tabela instituicao com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertUsuario(String nome, String login,  String senha, Date data_ingresso, String email, int id_instituicao){
+        String sql = "INSERT INTO usuario(nome,login,senha,data_ingresso,email,id_instituicao) VALUES (?,?,?,?,?,?)";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, nome);
+            stmt.setString(2, login);
+            stmt.setString(3, senha);
+            stmt.setDate(4, new java.sql.Date(data_ingresso.getTime()));
+            stmt.setString(5, email);
+            stmt.setInt(6, id_instituicao);
+            stmt.executeUpdate();
+            System.out.println("Dados inseridos na tabela instituicao com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
