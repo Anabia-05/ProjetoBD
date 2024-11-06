076a323d805779d6bf5a93498d13fafd0be8f162
Anabia-05 <anabia.romero@gmail.com>
2024-11-06 10:00:19 -0300

    ajustes de imports

--- ProjetoBD/src/DatabaseCreator.java
+++ ProjetoBD/src/DatabaseCreator.java
@@ -1,3 +1,8 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseCreator {
 
     private static final String URL = "jdbc:mysql://localhost:3306/";
