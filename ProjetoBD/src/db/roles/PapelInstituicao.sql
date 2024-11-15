7e23e58f50e69220d1929329f0ae7b67fa867df8
Anabia <ana.00000850038@unicap.br>
2024-11-15 19:01:20 -0300

    ajustes das roles para funcionamento correto

--- ProjetoBD/src/db/roles/PapelInstituicao.sql
+++ ProjetoBD/src/db/roles/PapelInstituicao.sql
@@ -1,9 +1,7 @@
--- Criar a role PapelInstituicao
 CREATE ROLE PapelInstituicao;
 
--- Conceder permissões de SELECT
 GRANT SELECT ON instituicao TO PapelInstituicao;
 GRANT SELECT ON usuario TO PapelInstituicao;
 GRANT SELECT ON arquivo TO PapelInstituicao;
 
-
+GRANT PapelInstituicao TO 'webdriver_user'@'localhost';
--- ProjetoBD/src/db/roles/PapelUsuario.sql
+++ ProjetoBD/src/db/roles/PapelUsuario.sql
@@ -1,4 +1,3 @@
-
 CREATE ROLE PapelUsuario;
 
 CREATE VIEW arquivos_usuario AS
@@ -8,4 +7,4 @@ SELECT * FROM arquivo WHERE id_usuario = :id_usuario;
 GRANT SELECT, INSERT, UPDATE ON arquivos_usuario TO PapelUsuario;
 
 
-
+GRANT PapelUsuario TO 'webdriver_user'@'localhost';
